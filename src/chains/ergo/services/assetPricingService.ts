4c7099d110abc8311d10f18aa1ae6fb0d9d3fdbd
ErgoTester <134221896+ErgoTester@users.noreply.github.com>
2026-01-21 15:28:34 +0200

    Update assetPricingService.ts

--- src/chains/ergo/services/assetPricingService.ts
+++ src/chains/ergo/services/assetPricingService.ts
@@ -1,6 +1,7 @@
 import { ERG_TOKEN_ID } from "@/constants/ergo";
 import { coinGeckoService } from "./coinGeckoService";
 import { spectrumService } from "./spectrumService";
+import { dexyService } from "./dexyService";
 
 const MIN_USD_LIQUIDITY = 1_000;
 
@@ -8,19 +9,26 @@ export type AssetRate = { erg: number; fiat: number };
 
 class AssetPricingService {
   async getRates(fiatCurrency: string): Promise<Map<string, AssetRate> | undefined> {
-    const [ergFiatRate, tokenRates] = await Promise.all([
+    const [ergFiatRate, tokenRates, dexyRates] = await Promise.all([
       coinGeckoService.getPrice(fiatCurrency),
-      spectrumService.getRatesByLiquidity(MIN_USD_LIQUIDITY)
+      spectrumService.getRatesByLiquidity(MIN_USD_LIQUIDITY),
+      dexyService.getRates()
     ]);
 
-    if (!ergFiatRate && !tokenRates) return undefined;
+    if (!ergFiatRate && !tokenRates && !dexyRates) return undefined;
 
     const rates = new Map<string, AssetRate>([[ERG_TOKEN_ID, { erg: 1, fiat: ergFiatRate }]]);
-    if (!tokenRates) return rates;
+    if (tokenRates) {
     for (const [key, value] of tokenRates) {
       rates.set(key, { erg: value.toNumber(), fiat: value.times(ergFiatRate).toNumber() });
+      }
     }
-
+    if (dexyRates) {
+    for (const [key, value] of dexyRates) {
+      rates.set(key, { erg: value.toNumber(), fiat: value.times(ergFiatRate).toNumber() });
+      }
+    }
+    
     return rates;
   }
 }
